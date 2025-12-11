c53fa0c9fd34701234572d4c045d21c97075d865
jsturgiss <jsturgiss@caretakerinc.com>
2025-12-11 10:48:10 -0700

    Added json helper with func for retrieving keys for serialization.

---
+++ CareAdApi/Helpers/JsonHelper.cs
@@ -0,0 +1,30 @@
+﻿using System.Reflection;
+using System.Text.Json.Serialization;
+
+namespace CareAdApi.Helpers
+{
+    public static class JsonHelper
+    {
+        public static string[] GetSerializedKeys<T>()
+        {
+            List<string> keys = new List<string>();
+
+            Type objType = typeof(T);
+            PropertyInfo[] props = objType.GetProperties();
+            foreach(PropertyInfo prop in props)
+            {
+                JsonPropertyNameAttribute? attr = prop.GetCustomAttribute<JsonPropertyNameAttribute>();
+                if(attr != null)
+                {
+                    keys.Add(attr.Name);
+                }
+                else
+                {
+                    keys.Add(prop.Name);
+                }
+            }
+
+            return keys.ToArray();
+        }
+    }
+}

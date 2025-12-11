a3527e2a685789099410f533477833bca39959a4
jsturgiss <jsturgiss@caretakerinc.com>
2025-12-11 10:00:34 -0700

    Added constants file.

---
+++ CareAdApi/Constants.cs
@@ -0,0 +1,24 @@
+﻿namespace CareAdApi
+{
+    public static class Constants
+    {
+        public const string ServiceName = "careapp-ad-api";
+        public const string Copyright = "Copyright (c) 2025 - Caretaker Landscape and Tree Management";
+        public const string LogTemplateName = "app_.log";
+        public const string ArgLogLevel = "--loglevel";
+        public const string DomainTarget = "caretaker.local";
+
+        public static class LoggingConstants
+        {
+            public const string ConsoleFormat = "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level:u3}] {Message:lj}{NewLine}{Exception}";
+            public const string FileFormat = "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level:u3}] {Message:lj}{NewLine}{Exception}";
+        }
+
+        public static class AttrConstants
+        {
+            public const string UserPrincipal = "userPrincipal";
+            public const string EmployeeId = "employeeId";
+            public const string ManagerPrincipal = "manager";
+        }
+    }
+}

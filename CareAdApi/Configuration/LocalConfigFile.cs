f33c867632e73add2069ce7347582b8a695a7571
jsturgiss <jsturgiss@caretakerinc.com>
2025-12-11 17:02:47 -0700

    Built out config file for securing values. Added props for https config.

--- CareAdApi/Configuration/LocalConfigFile.cs
+++ CareAdApi/Configuration/LocalConfigFile.cs
@@ -1,22 +1,88 @@
-﻿using System.Text.Json;
+﻿using System.Security.Cryptography;
+using System.Text;
+using System.Text.Json;
 using System.Text.Json.Serialization;
 
 namespace CareAdApi.Configuration
 {
     public class LocalConfigFile
     {
+        private static byte[] m_secret = [];
+
+        [JsonPropertyName("secret_key_path")]
+        public string SecretKey { get; set; } = string.Empty;
+
+        [JsonPropertyName("certificate")]
+        public string Certificate { get; set; } = string.Empty;
+
+        [JsonPropertyName("certificate_key")]
+        public string CertificateKey { get { return Encrypt(PlainCertificateKey); } set { PlainCertificateKey = Decrypt(value); } }
+
+        [JsonIgnore]
+        public string PlainCertificateKey { get; set; } = string.Empty;
+
         [JsonPropertyName("header_key")]
         public string HeaderKey { get; set; } = string.Empty;
 
+        private string Encrypt(string plainText)
+        {
+            byte[] input = Encoding.UTF8.GetBytes(plainText);
+            byte[] output = [];
+            byte[] iv = [];
+            using (Aes aes = Aes.Create())
+            {
+                aes.Key = m_secret;
+                iv = RandomNumberGenerator.GetBytes(16);
+                aes.IV = iv;
+                using (var encryptor = aes.CreateEncryptor())
+                {
+                    output = encryptor.TransformFinalBlock(input, 0, input.Length);
+                }
+            }
+
+            return string.Join('|', Convert.ToBase64String(output), Convert.ToBase64String(iv));
+        }
+
+        private string Decrypt(string encrypted)
+        {
+            string[] parts = encrypted.Split('|');
+            byte[] input = Convert.FromBase64String(parts[0]);
+            byte[] iv = Convert.FromBase64String(parts[1]);
+            byte[] output = [];
+            using (Aes aes = Aes.Create())
+            {
+                aes.Key = m_secret;
+                aes.IV = iv;
+                using (var decryptor = aes.CreateDecryptor())
+                {
+                    output = decryptor.TransformFinalBlock(input, 0, input.Length);
+                }
+            }
+
+            return Encoding.UTF8.GetString(output);
+        }
+
         public static LocalConfigFile? LoadFile()
         {
-            string file = GetFilePath();
-            if (!File.Exists(file))
+            string filePath = GetFilePath();
+            if (!File.Exists(filePath))
             {
                 return null;
             }
 
-            return JsonSerializer.Deserialize<LocalConfigFile>(File.ReadAllBytes(file)) ?? new LocalConfigFile();
+            string keyPath = GetKeyPath();
+            if (File.Exists(keyPath))
+            {
+                m_secret = File.ReadAllBytes(keyPath);
+            }
+            else
+            {
+                throw new FileNotFoundException("Could not find local encryption key.");
+            }
+
+            LocalConfigFile file = JsonSerializer.Deserialize<LocalConfigFile>(File.ReadAllBytes(filePath)) ?? new LocalConfigFile();
+
+            return file;
         }
 
         public static void SaveFile(LocalConfigFile file)
@@ -32,6 +98,11 @@ namespace CareAdApi.Configuration
             string file = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, Constants.LocalConfigFile);
 
             return file;
+        }
+
+        private static string GetKeyPath()
+        {
+            return Path.Combine(Path.GetDirectoryName(GetFilePath())!, Constants.KeyFileName);
         }
     }
 }
